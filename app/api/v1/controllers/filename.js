5883b0288829848791b6542a2daee29163d43f03
Okechukwu Daniel <okechukwudaniel12@gmail.com>
2024-10-23 23:06:56 +0100

    just commit

--- app/api/v1/controllers/filename.js
+++
@@ -1,9 +0,0 @@
-export const generateRandomString = () => {
-  const chars =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  let result = "";
-  for (let i = 0; i < 15; i++) {
-    result += chars.charAt(Math.floor(Math.random() * chars.length));
-  }
-  return result;
-};
