7d386c96978a0171703f6971ba92063327619a56
Okechukwu Daniel <okechukwudaniel12@gmail.com>
2024-10-23 11:07:27 +0100

    push data

--- components/Blog/blogDisplaycarousel.tsx
+++ components/Blog/blogDisplaycarousel.tsx
@@ -1,15 +1,28 @@
-import React from "react";
-import testImage from '@/Assets/testimage.webp'
+"use client"
+import React, { useState } from "react";
 import Image from "next/image";
+import { blogs } from "@/lib/blog.data";
+import { ChevronLeft } from "lucide-react";
 const BlogDisplaycarousel = () => {
+  const [imageIndex, setimageIndex] = useState(0)
+
   return (
-    <div className=" col-span-2 rounded-3xl overflow-hidden w-full h-[300px] md:h-[400px] mx-auto bg-gradient-to-r from-slate-950 to-slate-800">
+    <div className=" relative col-span-2 rounded-3xl overflow-hidden w-full bg-black h-[300px] md:h-[400px] mx-auto ">
         <Image
-    src={testImage}
+    src={blogs[imageIndex].coverPhoto}
     alt=''
-    className='h-full w-full   object-cover opacity-75 '
+    width={300}
+    height={300}
+    className='h-full w-full absolute  object-cover opacity-65 z-0'
    
     />
+    <div className=" w-full h-full z-0  absolute bg-gradient-to-r from-blue-500 to-transparent opacity-40"/>
+     <div className="relative">
+
+
+
+      <ChevronLeft className="relative cursor-pointer z-30 " onClick={()=>{setimageIndex(1)}}/>
+     </div>
     </div>
   );
 };
